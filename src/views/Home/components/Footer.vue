3f4b36603cb4e19ca43f7b58faf8f24190963d43
葛东亚 <gdy_work_9464@163.com>
2023-10-09 16:30:37 +0800

    footer

---
+++ src/views/Home/components/Footer.vue
@@ -0,0 +1,213 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="url-warp">
+        <div class="column location">
+          <div
+            style="cursor: pointer;"
+            @click="tohome"
+          >
+            <img
+              src="@/assets/images/logo.png"
+              alt="logo"
+              class="logo"
+            >
+            {{ last }}
+          </div>
+          <div
+            class="desc"
+            style="margin-top: 8px;"
+          >
+            邮箱：hi@huoxian.cn
+          </div>
+          <div class="desc">
+            电话：
+            <a href="tel:010-82772660">010-82772660</a>
+            <a href="tel:19910386797">199 1038 6797</a>
+          </div>
+          <div class="desc">
+            总部：北京市海淀区上地东路35号颐泉汇C座3层
+          </div>
+          <div class="desc">
+            分支机构：上海 深圳
+          </div>
+        </div>
+        <div class="column">
+          <div class="label">
+            产品与服务
+          </div>
+          <div class="url top-url">
+            企业资产发现
+          </div>
+          <div class="url">
+            企业风险识别
+          </div>
+        </div>
+        <div class="column">
+          <div class="label">
+            关于&帮助
+          </div>
+          <div class="url top-url">
+            关于我们
+          </div>
+          <div class="url">
+            帮助文档
+          </div>
+          <div class="url">
+            建议反馈
+          </div>
+        </div>
+        <div class="column">
+          <div class="label">
+            关注我们
+          </div>
+          <div class="qrCode">
+            <!-- <img
+              class="gongzhonghao"
+              src="@/assets/images/gongzhonghao.jpg"
+              alt="公众号"
+            > -->
+            <div class="desc">
+              火线安全平台
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright">
+        Copyright© 2022 Huoxian All rights reserved.
+        <span>丨</span>
+        <a
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+        >京ICP备20013659号-2</a>
+        <span>丨</span>北京安全共识科技有限公司
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    last: {
+      type: String,
+      default: '1'
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  watch: {
+    $route: {
+      handler: function(val, oldVal) {
+        console.log(this.$route.name)
+      },
+      immediate: true
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    tohome() {
+      this.$router.push('/')
+      this.$emit('lastChange', this.last + '1')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+footer {
+  background: #1B2332;
+  padding: 0 20px;
+  color: #ABABAB;
+
+  .url-warp {
+    padding-top: 40px;
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 24px;
+
+    .column {
+      img {
+        height: 32px;
+      }
+
+      .desc {
+        font-size: 12px;
+        line-height: 17px;
+
+        & + .desc {
+          margin-top: 11px;
+        }
+
+        a {
+          color: #ABABAB;
+
+          & + a {
+            margin-left: 16px;
+          }
+        }
+      }
+
+      .label {
+        color: #fff;
+        font-size: 14px;
+        line-height: 20px;
+      }
+
+      .top-url {
+        margin-top: 16px;
+      }
+
+      .url {
+        font-size: 12px;
+        line-height: 17px;
+        cursor: pointer;
+        display: block;
+        color: #ABABAB;
+
+        &:hover {
+          color: #d73533;
+        }
+
+        & + .url {
+          margin-top: 11px;
+        }
+      }
+
+      .gongzhonghao {
+        width: 72px;
+        height: 72px;
+        margin-top: 16px;
+      }
+    }
+  }
+
+  .copyright {
+    color: #ABABAB;
+    font-size: 12px;
+    padding-top: 24px;
+    border-top: 1px groove #ABABAB;
+    text-align: center;
+    padding-bottom: 36px;
+
+    a {
+      color: #ABABAB;
+    }
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  footer {
+    width: fit-content;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,44 +1,47 @@
 <template>
-  <el-container class="container">
-    <el-header class="home_header">
-      <div class="logo">
-        <h3>logo</h3>
-      </div>
-      <div class="menu">
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          active-text-color="#AC2807"
-          background-color="#F6F9FB"
-          router
-          @select="handleSelect"
-        >
-          <el-menu-item index="/home/act">首页</el-menu-item>
-          <el-menu-item index="/home/capability">产品能力</el-menu-item>
+  <main class="layout">
+    <el-container class="container">
+      <el-header class="home_header">
+        <div class="logo" @click="tohome">
+          <h3>logo</h3>
+        </div>
+        <div class="menu">
+          <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            active-text-color="#AC2807"
+            background-color="#F6F9FB"
+            router
+            @select="handleSelect"
+          >
+            <el-menu-item index="/home/act">首页</el-menu-item>
+            <el-menu-item index="/home/capability">产品能力</el-menu-item>
 
-          <el-menu-item index="/home/solution">解决方案</el-menu-item>
-          <el-menu-item index="/home/market">应用服务市场</el-menu-item>
-          <el-menu-item index="/home/developer">开发者</el-menu-item>
-          <el-menu-item index="/home/support">服务支持</el-menu-item>
-          <el-menu-item index="/home/national">了解国能云</el-menu-item>
-          <el-menu-item index="/home/coal">煤炭行业云</el-menu-item>
-        </el-menu>
-      </div>
-      <div class="search">
-        <h3>搜索</h3>
-      </div>
-      <div class="main">
-        <h3>个人</h3>
-      </div>
-    </el-header>
-    <el-main style="height:500px">
-      <router-view />
-    </el-main>
-    <el-footer class="home_footer" style="height:400px">
+            <el-menu-item index="/home/solution">解决方案</el-menu-item>
+            <el-menu-item index="/home/market">应用服务市场</el-menu-item>
+            <el-menu-item index="/home/developer">开发者</el-menu-item>
+            <el-menu-item index="/home/support">服务支持</el-menu-item>
+            <el-menu-item index="/home/national">了解国能云</el-menu-item>
+            <el-menu-item index="/home/coal">煤炭行业云</el-menu-item>
+          </el-menu>
+        </div>
+        <div class="search">
+          <h3>搜索</h3>
+        </div>
+        <div class="main">
+          <h3>个人</h3>
+        </div>
+      </el-header>
+      <el-main style="height:500px">
+        <router-view />
+      </el-main>
+      <Footer :last="test" @lastChange="lastChange" />
+    <!-- <el-footer class="home_footer" style="height:400px">
       <div class="home_footer_content" />
-    </el-footer>
-  </el-container>
+    </el-footer> -->
+    </el-container>
+  </main>
 </template>
 
 <script>
@@ -46,15 +49,21 @@ import { mapGetters } from 'vuex'
 import echarts from 'echarts'
 require('echarts/theme/macarons') // echarts theme
 import { debounce } from '@/utlis'
+import Footer from './components/Footer.vue'
 export default {
-  name: 'Vue',
+  name: 'Index',
+  components: {
+    Footer
+  },
   props: {},
   data() {
     return {
       chart: null,
       resizeHandler: null,
       radio1: '今日',
-      datevalue1: ''
+      datevalue1: '',
+      activeIndex: '',
+      test: 'footer'
     }
   },
   computed: {
@@ -65,6 +74,12 @@ export default {
   watch: {
     sidebarOpened() {
       this.resizeHandler()
+    },
+    $route: {
+      handler: function(val, oldVal) {
+        this.activeIndex = this.$route.path
+      },
+      immediate: true
     }
   },
   mounted() {
@@ -74,10 +89,19 @@ export default {
         _this.chart.resize()
       }
     }, 100)
-    _this.initChart()
+    // _this.initChart()
     _this.initResizeEvent()
   },
   methods: {
+    tohome() {
+      // console.log(111)
+    },
+    lastChange(newTest) {
+      this.test = newTest
+    },
+    handleSelect() {
+
+    },
     initChart() {
       this.chart = echarts.init(document.getElementById('home-traffic-chart'), 'macarons')
       this.chart.setOption({
@@ -106,6 +130,10 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.layout {
+  background: #f6f6f6;
+  min-height: 100vh;
+}
 .container{
   background: #F6F9FB;
 }
@@ -114,10 +142,12 @@ export default {
   /* justify-content: space-around; */
 }
 .logo {
-  flex-grow: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
   background: red;
-  text-align: center;
-  line-height: 50%;
+  flex-grow: 1;
 }
 .menu {
   flex-grow: 3;
@@ -129,13 +159,18 @@ export default {
   flex-grow: 4;
   background: green;
   text-align: center;
-  line-height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .main {
   flex-grow: 3;
   background: yellow;
   text-align: center;
   line-height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .el-menu{
   padding-left: 8%;
